edcdff90f5c44bae8a1257cba8dfbe94c2c77896
ya_feng_li <ya_feng_li@163.com>
2016-11-15 12:05:13 +0800

    init.

---
+++ src/main/g8/project/Build.scala
@@ -0,0 +1,11 @@
+
+import sbt._
+import scala.io.Source
+
+object Build {
+
+  var reg = "(.+)=(.+)".r
+
+  val $ = Source.fromFile("project/build.properties").getLines().filter(reg.findFirstMatchIn(_).isDefined).map(reg.findFirstMatchIn(_).get).map(m => m.group(1) -> m.group(2)).toMap
+  
+}
