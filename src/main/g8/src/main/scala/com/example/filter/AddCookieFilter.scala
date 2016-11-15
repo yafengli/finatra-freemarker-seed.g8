da84b4ab706233a6c38c8a07ca199d757811ad23
ya_feng_li <ya_feng_li@163.com>
2016-11-15 14:08:14 +0800

    is working.

--- src/main/g8/src/main/scala/com/example/filter/AddCookieFilter.scala
+++ src/main/g8/src/main/scala/com/example/filter/AddCookieFilter.scala
@@ -14,12 +14,12 @@ class AddCookieFilter extends SimpleFilter[Request, Response] {
   }
 
   private def trace(request: Request): Unit = {
-    println(s"---------Query Parameters:${request.params.size}"---------
-    request.params.foreach { t => println(s"${t._1}:${t._2}") }
-    println(s"---------Request Cookies:${request.cookies.size}---------")
+    println(s"---------Query Parameters:"+request.params.size+"---------")
+    request.params.foreach { t => println(t._1+":"+t._2 }
+    println(s"---------Request Cookies:"+request.cookies.size+"---------")
     request.cookies.foreach { t => println(s"${t._1}:${t._2.value}") }
-    println(s"---------Response Cookies:${request.response.cookies.size}---------")
-    request.response.cookies.foreach { t => println(s"${t._1}:${t._2.value}") }
+    println(s"---------Response Cookies:"+request.response.cookies.size+"---------")
+    request.response.cookies.foreach { t => println(t._1+":"+t._2) }
     println("-----------------------------------------------")
   }
 }
