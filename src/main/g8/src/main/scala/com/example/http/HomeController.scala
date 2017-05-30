99911e339d00eee364c9f817abffee16be7e6c52
ya_feng_li@163.com <ya_feng_li@163.com>
2017-05-30 09:47:24 +0800

    1

--- src/main/g8/src/main/scala/com/example/http/HomeController.scala
+++ src/main/g8/src/main/scala/com/example/http/HomeController.scala
@@ -19,14 +19,6 @@ class HomeController @Inject()(service: ExampleService, freemarkerConfigurationF
   get("/demo") { _: Request =>
     DemoView("YaFengLi", List(Person("test_1", 1), Person("test_2", 2), Person("test_3", 3)))
   }
-  get("/") { request: Request =>
-    response.ok.html(
-      s"""<h1>Hello ${request.ctx}!</h1>
-         |params:${request.params.map { t => t._1 + ":" + t._2 }.mkString("<br/>")}
-         |<br/>cookies:${request.cookies.map { t => t._1 + ":" + t._2.value }.mkString("<br/>")}
-         |<br/>Freemarker:<a href="/demo"> Freemarker Demo Page!</a>
-       """.stripMargin)
-  }
 
   post("/upload") { request: Request =>
     RequestUtils.multiParams(request).filter(_._1 == "file").headOption match {
